f39c538ad12c22e698be17c50454a23383361974
Irene Antoniazzi <ia8204@gmail.com>
2018-01-11 20:39:39 +0000

    Update FoodItem.java
    Removed lines referencing local java project

--- FoodItem.java
+++ FoodItem.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package restaurant;
 
-/**
- *
- * @author syntel
- */
 public class FoodItem {
     private static int FoodItemId;
     private static String Name;
