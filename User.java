e83f7ccfb37b7f4f84a8eba0029733c5f0abe28a
Irene Antoniazzi <ia8204@gmail.com>
2018-01-11 20:42:13 +0000

    Update User.java
    Removed lines referencing local java project

--- User.java
+++ User.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package restaurant;
 
-/**
- *
- * @author syntel
- */
 public class User {
     private static int UserId;
     private static String FirstName;
