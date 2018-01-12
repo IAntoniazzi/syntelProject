4d57acd722f109fd4b65e367c3abb9886e7bbdb8
Irene Antoniazzi <ia8204@gmail.com>
2018-01-12 21:56:47 +0000

    Update OrdersView.java

--- OrdersView.java
+++ OrdersView.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 
 
 public class OrdersView {
-    public void printOrders(int OrderId, int UserId, int AddressId, String PaymentMethod, String Date, String Time, float Price, ArrayList<Integer> Items){
+    public void printOrders(int OrderId, int UserId, int AddressId, String PaymentMethod, String oDate, String dDate, String Time, float Price, ArrayList<FoodItem> Items){
       System.out.println("Order Numer: "+ OrderId);
       System.out.println("For User: " + UserId);
       System.out.println("To Address: " + AddressId);
       System.out.println("Payment Method: " + PaymentMethod);
-      System.out.println("On: " + Date + " at " + Time);
+      System.out.println("Ordered On: " + oDate);      
+      System.out.println("Scheduled for Delivery On: " + dDate + " at " + Time);
       System.out.println("Price: $" + Price);
       System.out.println("Items: ");
       for(int i=0; i<Items.size(); i++){
