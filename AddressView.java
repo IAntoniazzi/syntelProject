a47227e063b9e1be94f0345f6b93e6c7e2ec0b49
Irene Antoniazzi <ia8204@gmail.com>
2018-01-12 20:45:49 +0000

    Update AddressView.java

--- AddressView.java
+++ AddressView.java
@@ -1,6 +1,6 @@
 
 public class AddressView {
-   public void printAddress(String Street1, String Street2, String City, String State, String Zip){
+   public void printAddress(String Street, String City, String State, String Zip){
       System.out.println("Address: ");
       System.out.println(Street1 + " "+ Street2);
       System.out.println(City + ", " + State + " "+ Zip);
