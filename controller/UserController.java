c6c2907646775dc3647a2ea3cd2eff02b6788667
kingoruga <koruga@ggc.edu>
2018-01-15 00:56:00 -0700

    Login and registration implemented

--- controller/UserController.java
+++ controller/UserController.java
@@ -6,6 +6,7 @@
 package controller;
 
 import model.UserInfo;
+import view.LoginView;
 
 /**
  *
@@ -14,40 +15,58 @@ import model.UserInfo;
 public class UserController {
 
     private String email;
-    UserInfo info;
-
-    public String getEmail() {
-        return email;
-    }
+    UserInfo info = new UserInfo();
+    LoginView loginResponse = new LoginView();
 
     public void updateModelEnableUser(String userEmail) {
-        info = new UserInfo();
         info.adminEnableUser(email);
     }
 
     public void updateModelDisableUser(String email) {
-        info = new UserInfo();
         info.adminDisableUser(email);
     }
 
     public void updateModelDeleteUser(String email) {
-        info = new UserInfo();
         info.adminDeleteUser(email);
     }
 
-    public void login(String email, String password) {
-        info.login(email, password);
-        info.isAdminOrCustomer(email, password);
-
-        if (info.isAdminOrCustomer(email, password).equals("Admin")) {
-
-        }
-
+    public void updateModelChangePassword(String userEmail, String password) {
+        info.adminChangeUserPassword(userEmail, password);
     }
 
-    public void updateModelChangePassword(String userEmail, String password) {
+    public void userControllerLogin(String email, String password) {
         info = new UserInfo();
-        info.adminChangeUserPassword(userEmail, password);
+        loginResponse = new LoginView();
+        String result = info.loginUser(email, password);
+        if(result.equals(" ")){
+            loginResponse.printUpdatedResponse(7);
+        }
+        else if(result.equals("admin")){
+            loginResponse.adminUser();
+        }
+        else
+             loginResponse.nonAdminUser(result);
+    }
+
+    public void userControllerRegister(String fname, String lname, String email, String passWrd, String strAddress, 
+                                                     String city, String state, int zipCode) {
+        info = new UserInfo();
+        info.registerNewUser( fname, lname, email, passWrd, strAddress, city, state, zipCode);
+    }
+
+
+
+    public void userSuccessfullyUpdated(int i) {
+        switch (i) {
+            case 0: loginResponse.printUpdatedResponse(0); break;
+            case 1: loginResponse.printUpdatedResponse(1); break;
+            case 2: loginResponse.printUpdatedResponse(2); break;
+            case 3: loginResponse.printUpdatedResponse(3); break;
+            case 4: loginResponse.printUpdatedResponse(4); break;
+            case 5: loginResponse.printUpdatedResponse(5); break;
+            case 6: loginResponse.printUpdatedResponse(6); break;
+            default: break;
+        }
     }
 
 }
--- view/LoginView.java
+++ view/LoginView.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import controller.UserController;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
@@ -12,11 +15,90 @@ import java.util.Scanner;
  * @author syntel
  */
 public class LoginView {
+
+    UserController userController;
+    MealOptionsView mealOptions = new MealOptionsView();
     
-    public void login(String email, String password){
-       //userControllerLogin(email, password);
+    public void login(String email, String password) {
+       userController  = new UserController();
+        userController.userControllerLogin(email, password);
     }
     
+    public void register(String fname, String lname, String email, String passWrd, String strAddress, String city, String state, int zipCode) {
+        userController  = new UserController();
+        userController.userControllerRegister(
+                fname, lname, email, passWrd, strAddress, city, state, zipCode);
+    }
 
-    
+    public void adminUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome, admin!");
+
+        System.out.println("Enter 5 to enable user");
+        System.out.println("Enter 6 to disable user");
+        System.out.println("Enter 7 to delete user");
+        System.out.println("Enter 8 to change user password");
+        System.out.println("Enter 9 to go back to main menu");
+        System.out.println("Enter 10 to exit");
+        int response = input.nextInt();
+
+        if (response == 5) {
+            System.out.println("Enter User Email to enable");
+            String userEmail = input.next();
+            userController.updateModelEnableUser(userEmail);
+        }
+
+        if (response == 6) {
+            System.out.println("Enter User Email to disable");
+            String userEmail = input.next();
+            userController.updateModelDisableUser(userEmail);
+        }
+
+        if (response == 7) {
+            System.out.println("Enter User Email to delete");
+            String userEmail = input.next();
+            userController.updateModelDeleteUser(userEmail);
+        }
+
+        if (response == 8) {
+            System.out.println("Enter User Email to change password");
+            String userEmail = input.next();
+            System.out.println("Enter new password");
+            String newPassword = input.next();
+            userController.updateModelChangePassword(userEmail, newPassword);
+        }
+
+        if (response == 10) {
+            System.exit(0);
+        }
+    }
+
+    public void nonAdminUser(String name) {
+        //From here, user can see options to order a meal or package
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate localDate = LocalDate.now();
+        System.out.println(dtf.format(localDate));
+        System.out.println("\nHello, " + name + ". You are logged, in.");
+        System.out.println("\nHere are the Menu Items for today, " + dtf.format(localDate) +".");
+        System.out.println("---------------------------------");
+        mealOptions.getMenuItems();
+
+    }
+
+    public void printUpdatedResponse(int i) {
+        switch(i){
+            case 0: System.out.println("User successfully disabled!\n"); break;
+            case 1: System.out.println("User successfully enabled!\n"); break;
+            case 2: System.out.println("User successfully deleted!\n"); break;
+            case 3: System.out.println("Password changed successfully!\n"); break;
+            case 4: System.out.println("Email already exists. Please try another one.\n"); break;
+            case 5: System.out.println("You have successfully registered!\n"); break;
+            case 6: System.out.println("Email already exists. Please try another one.\n"); break;
+            case 7: System.out.println("Invalid email and password combination.\n"); break;
+            default: break;
+        }
+    }
+
+ 
+
 }
