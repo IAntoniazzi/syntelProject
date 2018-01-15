bc652c3e8f935afa217c381605c66262ec24fab6
syntel <syntel@PHX440G3-28161F.syntelorg.com>
2018-01-15 00:03:35 -0700

    Updated imports and packages

--- controller/AreaManagementController.java
+++ controller/AreaManagementController.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.syntel.Controller;
+package controller;
 
-import com.syntel.Models.AreaModel;
+import src.com.syntel.Models.AreaModel;
 
 /**
  *
--- src/com/syntel/Models/AreaModel.java
+++ src/com/syntel/Models/AreaModel.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.syntel.Models;
+package src.com.syntel.Models;
+
+import model.Connector;
 
 /**
  *
